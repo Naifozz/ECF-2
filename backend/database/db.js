bf48f96d195c14eddfa6f9c607ebbb549f72d6c0
Sofian De Braeckelaer <sdebraeckelaer@gmail.comgit config --global user.email sdebraeckelaer@gmail.>
2025-04-09 10:55:26 +0200

    feat(database): seeding database when opening db

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,44 +1,45 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import { readFile } from "fs/promises";
-import path from "path";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { seedDatabase } from './seed.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export async function openDb() {
-    try {
-        let dbPath;
+  try {
+    let dbPath;
 
-        if (process.env.NODE_ENV === "test") {
-            dbPath = path.join(__dirname,"database_test.db");
-        } else {
-            dbPath = path.join(__dirname, "database.db");
-        }
-
-        const db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database,
-        });
-        await initDb(db);
-
-        return db;
-    } catch (error) {
-        throw new Error("Failed to open database: " + error.message);
+    if (process.env.NODE_ENV === 'test') {
+      dbPath = path.join(__dirname, 'database_test.db');
+    } else {
+      dbPath = path.join(__dirname, 'database.db');
     }
+
+    const db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    await initDb(db);
+    await seedDatabase(db);
+    return db;
+  } catch (error) {
+    throw new Error('Failed to open database: ' + error.message);
+  }
 }
 
 async function initDb(db) {
-    try {
-        const __dirname = dirname(fileURLToPath(import.meta.url));
+  try {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
 
-        const schemaSQL = await readFile(join(__dirname, "schema.sql"), 'utf8');
+    const schemaSQL = await readFile(join(__dirname, 'schema.sql'), 'utf8');
 
-        await db.exec(schemaSQL);
-        console.log("Database initialized successfully with schema.sql");
-    } catch (error) {
-        throw new Error("Failed to initialize database");
-    }
+    await db.exec(schemaSQL);
+    console.log('Database initialized successfully with schema.sql');
+  } catch (error) {
+    throw new Error('Failed to initialize database');
+  }
 }
