78cc7ebcb594bb67d9d194d7e8172cbc0b8751b2
Sofian De Braeckelaer <sdebraeckelaer@gmail.comgit config --global user.email sdebraeckelaer@gmail.>
2025-09-25 13:05:35 +0200

    feat(jwt): changed express session to jwt

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,6 +1,18 @@
-export const isAuthenticated = (req, res, next) => {
-  if (req.session.isLoggedIn && req.session.user) {
-    return next();
+import jwt from 'jsonwebtoken';
+const SECRET_KEY = process.env.SECRET_KEY || 'exemple_clef_secrete';
+
+export const isAuthenticated = (req) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return false;
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, SECRET_KEY);
+    req.user = payload;
+    return true;
+  } catch (err) {
+    return false;
   }
+
   return res.status(401).json({ message: 'Authentification requise' });
 };
