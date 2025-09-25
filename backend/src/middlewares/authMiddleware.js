5c5916b68b641ac78c03cd3071be545b03abc364
Sofian De Braeckelaer <sdebraeckelaer@gmail.comgit config --global user.email sdebraeckelaer@gmail.>
2025-09-25 17:08:23 +0200

    fix(authmMiddleware): corrected function

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,16 +2,19 @@ import jwt from 'jsonwebtoken';
 const SECRET_KEY = process.env.SECRET_KEY || 'exemple_clef_secrete';
 
 export const isAuthenticated = (req) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) return false;
-
-  const token = authHeader.split(' ')[1];
-  if (!token) return false;
   try {
-    const payload = jwt.verify(token, SECRET_KEY);
-    req.user = payload;
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) return false;
+
+    const token = authHeader.substrin(7);
+
+    if (!token) return false;
+
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.user = decoded;
     return true;
   } catch (err) {
+    console.error('Erreur de vérification du token:', error);
     return false;
   }
 
